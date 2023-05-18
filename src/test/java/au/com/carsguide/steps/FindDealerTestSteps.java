03681d778a6bd106ec25a251600f1b5cf0d75ba4
bhavisha2017 <yellowjasmine786@gmail.com>
2023-05-18 23:35:09 +0100

    Bhavisha wed project

--- src/test/java/au/com/carsguide/steps/FindDealerTestSteps.java
+++ src/test/java/au/com/carsguide/steps/FindDealerTestSteps.java
@@ -6,37 +6,35 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class FindDealerTestSteps {
 
     @Given("^I am on car guide homepage$")
     public void iAmOnCarGuideHomepage() {
     }
+
     @When("^I mouse hover on “buy\\+sell” link$")
     public void iMouseHoverOnBuySellLink() {
         new HomePage().mouseOverToBuySell();
-//        hello
     }
 
     @And("^I click Find a Dealer$")
         public void iClickFindADealer() {
             new CarsDealershipPage().clickOnFindDealer();
         }
-    @And("^I navigate to 'Find a dealer'$")
-    public void iNavigateToFindADealer() {
-//        String expectedMessage = "car-dealers";
-//        String actualMessage = new CarsDealershipPage().getTextCarDealerShip();
-//        Assert.assertEquals(expectedMessage, actualMessage, "car-dealers");
-    }
 
+    @And("^I navigate to Find a dealer$")
+    public void iNavigateToFindADealer() {
+        String expectedMessage = "Find a Car Dealership Near You";
+        String actualMessage = new CarsDealershipPage().getTextCarDealerShip();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
 
     @Then("^I should see the dealers name \"([^\"]*)\" on page$")
     public void iShouldSeeTheDealersNameOnPage(String dealersName ) throws InterruptedException {
         new CarsDealershipPage().findDealerName(dealersName);
     }
-
-
-
 }
 
 
--- src/test/java/resources/featurefile/finddealers.feature
+++ src/test/java/resources/featurefile/finddealers.feature
@@ -5,13 +5,13 @@ Feature: Find the dealers and verify
     Given I am on car guide homepage
     When  I mouse hover on “buy+sell” link
     And   I click Find a Dealer
-    And  I navigate to 'Find a dealer'
+    And  I navigate to Find a dealer
     Then I should see the dealers name "<dealersName>" on page
     Examples:
       | dealersName             |
       | 3 Point Motors Epping   |
-      | 5 Star Auto             |
-      | A & M Car Sales Pty Ltd |
-      | A1 MOTORS COMPANY       |
-      | ANDREA MOTORI SERVICE   |
-      | Action Motors           |
+#      | 5 Star Auto             |
+#      | A & M Car Sales Pty Ltd |
+#      | A1 MOTORS COMPANY       |
+#      | ANDREA MOTORI SERVICE   |
+#      | Action Motors           |
